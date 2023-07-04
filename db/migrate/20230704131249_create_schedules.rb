234c2446ce4809a82cd31292116fcdfa811feb1a
tanaka <tanaka.for.public@gmail.com>
2023-07-04 23:26:13 +0900

    first PR

---
+++ db/migrate/20230704131249_create_schedules.rb
@@ -0,0 +1,13 @@
+class CreateSchedules < ActiveRecord::Migration[7.0]
+  def change
+    create_table :schedules do |t|
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.text :memo
+      t.boolean :all_day
+
+      t.timestamps
+    end
+  end
+end
